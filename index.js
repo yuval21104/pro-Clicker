63a9e971696a2de06e1269ead936e324740effc4
yuval21104 <127964202+yuval21104@users.noreply.github.com>
2023-06-29 18:23:31 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+function decrement(){
+    if (count > 0){
+        count--
+    }
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
+
+function clearClicksHistory(){
+    saveEl.textContent = "Previous clicks: "
+}
+
